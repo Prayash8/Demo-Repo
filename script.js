b5c65e44b2d573852a5c3c720cea2d7314f94b40
Prayash8 <prayash33711@gmail.com>
2024-04-01 12:00:42 +0545

    latest

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 // You can add JavaScript functionality here if needed
 console.log("Script loaded");
+// JavaScript for fancy button
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', function() {
+    alert('Button clicked!');
+});
+
+// JavaScript for interactive rapper names
+const rapperNames = document.querySelectorAll('#rapper-list li');
+rapperNames.forEach(function(rapper) {
+    rapper.addEventListener('click', function() {
+        const rapperName = rapper.textContent;
+        alert(`You clicked on ${rapperName}. Redirecting to their YouTube channel...`);
+    });
+});
 
